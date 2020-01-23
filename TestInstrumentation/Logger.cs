c52c31d4be4e93420bfcd66470d99686f9df4dc2
amberlu00 <amberlu00@gmail.com>
2020-01-22 19:13:37 -0600

    * TestInstrumentation.sln: Added Serilog logger
    * Logger.cs:
    * Tester.cs:
    * RequestHandler.cs:
    * TestInstrumentation.csproj:

---
+++ TestInstrumentation/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Serilog;
+
+namespace TestInstrumentation
+{
+    public class Logger
+    {
+
+        public Logger(string appType)
+        {
+            Log.Logger = new LoggerConfiguration().
+                WriteTo.File(string.Concat(appType, ".txt"),
+                rollingInterval: RollingInterval.Day).CreateLogger();
+        }
+
+        public void LogStatus(string info) {
+            Log.Information(string.Concat("{Now}", info), DateTime.Now);
+        }
+
+        public void LogError(string info)
+        {
+            Log.Error(string.Concat("{Now}", info), DateTime.Now);
+        }
+    }
+}
---
+++ TestInstrumentation/Tester.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestInstrumentation
+{
+    public class Tester
+    {
+        public static void Main(string[] args)
+        {
+            // Examples of general logging.
+            Logger pLogger = new Logger("Producer");
+            pLogger.LogError("Everything broke!!!!");
+            Logger cLogger = new Logger("Consumer");
+            cLogger.LogStatus("Everything is OK!!!");
+
+            // Examples of specific logging.
+
+        }
+    }
+}
