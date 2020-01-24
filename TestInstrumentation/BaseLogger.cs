692c80517a1cac6c2d5a29f45c2e5ba2b172eeb3
amberlu00 <amberlu00@gmail.com>
2020-01-23 22:48:36 -0600

    * BaseLogger.cs: Changed namespace
    * CustomLogger.cs:

--- TestInstrumentation/BaseLogger.cs
+++ TestInstrumentation/BaseLogger.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Serilog;
-using System.Collections.Generic;
 
 namespace TestInstrumentation
 {
--- TestInstrumentation/CustomLogger.cs
+++ TestInstrumentation/CustomLogger.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using TestInstrumentation;
 
-namespace Application
+namespace TestInstrumentation
 {
     public class CustomLogger
     {
